32f8631b2544999ad7997dd3b2a93eabd0e347b0
McDony Lee <mcdonyleee@gmail.com>
2022-08-13 19:57:13 +1000

    Ver 1.0.6 Early DevNote: Polished simple end game logic

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MekdieJack
-A copy of blackjack with mekdie's style and rule ! This game was aimed to copy famous BlackJack card game by doing 1v1 against AI. Pure luck or skill is on you~ The reason MekdieJack is created is no other than to improve my JavaScript and coding skils.
+A copy / parody of blackjack with Mekdie's style and rule ! This game was aimed to copy famous BlackJack card game by doing 1v1 against AI. Pure luck or skill is on you~ The reason MekdieJack is created is no other than to improve my JavaScript and coding/algorithm skills. The game can be accessed [here](https://mekdie.github.io/MekdieJack/):
+
+External link: https://mekdie.github.io/MekdieJack/ 
 
 ## Version Changelog
 *Version 1.0 Early DevNote:*
@@ -21,13 +23,12 @@ A copy of blackjack with mekdie's style and rule ! This game was aimed to copy f
 
 *Version 1.0.4 Early DevNote:*
 - Add a sound effect whenever a game starts, restarts, or dealing cards
-- What AI's aspect are currently needed to be implemented:
-    - The game must keep going after the player has decided to stay
-    - The AI must think to beat the player or loses to the player depends on the circumstances (e.g. need to keep going if the dealer/ai's card is not yet equal to 18)
+- What AI's aspect are currently needed to be implemented: [done]
+    - The game must keep going after the player has decided to stay 
+    - The AI must think to beat the player or loses to the player depends on the circumstances (e.g. need to keep going if the dealer/ai's card is not yet equal to 18) 
     - Dealer wins automatically if it reaches 21
-    - Dealer loses automatically if its card exceeds 21
-    - Reveal dealer card one by one as using timeout (maybe) if hit button for dealer is clicked (they play)
-    - Sound effect when win / lose
+    - Dealer loses automatically if its card exceeds 21 
+    - Reveal dealer card one by one as using timeout (maybe) if hit button for dealer is clicked (they play) 
     - Disable the buttons after the game ends (need to restart)
     - To be continued ...
 
@@ -35,3 +36,17 @@ A copy of blackjack with mekdie's style and rule ! This game was aimed to copy f
 - Added basic AI function to keep it playing until either win or lose 
 - Added auto play for AI (AI Press their own hit button) 
 - TO BE CONTINUED (refer to the commented code)
+- Changed version name from Alpha to Early DevNote means a version note to keep track of changes between each development process.
+
+*Version 1.0.6 Early DevNote:*
+- Update the description a bit
+- Added word break on the player and dealer (to prevent overflowing)
+- Disable play hit and stay button if either win or lose
+- Added draw if both total are the same
+- Added logic where dealer wins automatically if it reaches 21
+- Added logic where dealer loses if its card exceeds 21
+- Remove unnecessary code
+- Change restart button text to 'Play again' after the game finishes
+- Next:
+    - Add Randomizer between dealer's threshold either 16-17-18-19
+    - Add Betting or Score system
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var playerSum = 0;
 var dealerSum = 0;
 var message = '';
 
-//sound effect when clicking button
+// === sound effect when clicking button
 
 const dealingCard = new Audio("audio/dealingCard.wav");
 dealingCard.playbackRate = 2;
@@ -23,7 +23,7 @@ shuffleBtn.click(function() {
     shufflingCard.play();
 })
 
-//
+// ===========
 
 function sumTotal(arr) {
     let i;
@@ -73,6 +73,7 @@ function startGame() {
 }
 
 function restartGame() {
+    $('#restart').html('Restart');
     $('#result').html('');
     $('#hit').prop('disabled', false);
     $('#stay').prop('disabled', false);
@@ -99,81 +100,46 @@ function hitBtn() {
     if (playerSum == 21) {
         $('#result').html('You Win');
         disableBtn();
-        revealAI();
     }
     else if (playerSum > 21) {
         $('#result').html('You Lose');
         disableBtn();
-        revealAI();
     }
 }
-// function aiClick() {
-//     $('#stay').click();
-// }
 function stayBtn() {
+    disableBtn();
     getCardAI();    
-    console.log(dealerSum);
-    //AI AUTO PLAY TRICKED WITH CLICK, it mimics the human click so it will keep playing if they haven't reach 18.
-    //BASIC ALGORITHM AND WILL BE COMBINED WITH COMMENTED CODE BELOW
+    
+    //this is going to the loop because, after click stay btn, the inside of the timeout is clicking the stay button itself, 
+    // so it will keep repeating as a recursive function until certain conditions met
+    
+    //AI keep playing if the sum is not equal to 18 
     if (dealerSum <= 18) {
         setTimeout(function() {
             $('#stay').click();
         }, 1000);
     }
+    // stop playing if exceeds 18
     else {
-        console.log("finish");
-    }
-
-
-    // var flag = false;
-    // while (!flag) {
-    //     if (dealerSum <= 21) {
-    //         //THE REASON IT GETS BACK HERE IS BECAUSE 
-    //         getCardAI();
-    //         // HAVE TO COUNT THE TOTAL HERE
-    //         console.log(dealerSum);
-    //         //if the dealer sum equal or less than 17, keep going
-    //         if (dealerSum <= 18) {
-    //             setTimeout(getCardAI, 1000);
-    //             console.log('second card');
-    //             continue;
-    //         }
-    //         else {
-    //             if (dealerSum > playerSum) {
-    //                 $('#result').html('You Lose');
-    //                 disableBtn();
-    //                 revealAI();
-    //                 flag = true;
-    //                 console.log("first if");
-    //             }
-    //             else {
-    //                 $('#result').html('You Win');
-    //                 disableBtn();
-    //                 flag = true;
-    //                 console.log("second if");
-    //             }
-    //         }
-    //     }
-    //     //automatically win if the dealer sum more than 21
-    //     else {
-    //         $('#result').html('You Win');
-    //         disableBtn();
-    //         flag = true;
-    //         console.log("third if");
-    //     }   
-    // console.log(flag);
-    // // console.log(dealerSum);
-    // }    
-    // console.log("outside while");
-    // console.log("==========")
+        if ((dealerSum > playerSum) && dealerSum <= 21) {
+            $('#result').html('You Lose');
+            $('#restart').html('Play again');
+            disableBtn();
+        }
+        else if (dealerSum == playerSum) {
+            $('#result').html('Draw');
+            $('#restart').html('Play again');
+            disableBtn();  
+        }
+        else {
+            $('#result').html('You Win');
+            $('#restart').html('Play again');
+            disableBtn();   
+        }
+    }    
 }
 
 function disableBtn() {
     $('#hit').prop('disabled', true);
     $('#stay').prop('disabled', true);
-}
-
-function revealAI() {
-    $('#dealer-cards').html('<p id="initialAI">'+dealerCards+'</p>');
-    $('#dealer-sum').html(dealerSum);
 }

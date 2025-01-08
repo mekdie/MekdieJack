a4331a64d2730a9957eff3722fd513f02bcaa63c
McDony Lee <mcdonyleee@gmail.com>
2025-01-08 22:04:07 +1100

    Code improvement and bug fix when the game has not started yet

--- index.js
+++ index.js
@@ -34,127 +34,94 @@ stayBtn.click(function () {
 
 // ===========
 // print function to debug
-function print(val) {
-    return console.log(val);
-}
+const print = (val) => console.log(val);
 
-function sumTotal(arr) {
-    let i;
-    let sum = 0;
-    for (i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum;
-}
-function getCard() {
-    var card = cards[Math.floor(Math.random() * cards.length)];
-    // print(chance);
-    // if the player has reached 16 or more
+const sumTotal = (arr) => arr.reduce((sum, value) => sum + value, 0);
 
-    // rule no.1: they got 10% chance to reach 21
-    // rule no.2: they got 80% chance to reach 20 or 21
-    // rule no.3: they got 10% chance to exceed 21
+const getCard = () => {
+    let card = cards[Math.floor(Math.random() * cards.length)];
+    const chance = Math.random();
+
     if (playerSum >= 16) {
-        var chance = Math.random();
-        //10% chance to reach 21 WIN DIRECTLY
         if (chance <= 0.1) {
+            // 10% chance to reach 21
             console.log("10% chance to win instantly");
             card = 21 - playerSum;
-            // print(card);
-        }
-        //80% chance to go here
-        else if (chance <= 0.9) {
-            //must put the condition if playerSum == 20
+        } else if (chance <= 0.9) {
+            // 80% chance to go here
             console.log("80% chance");
-            var inner80 = Math.random();
-            console.log(inner80);
-            //if the value is more than 16 but no equal to 20
-            if (playerSum != 20) {
-                // rule no.1: 10% chance to get 21
-                if (inner80 <= 0.1) {
-                    //10%
+            const innerChance = Math.random();
+            console.log(innerChance);
+
+            if (playerSum === 20) {
+                // 10% chance to win directly to 21
+                if (innerChance <= 0.1) {
+                    console.log("win from 10% chance");
+                    card = 1;
+                } else {
+                    // 90% chance to lose, sum will exceed 21
+                    console.log("lose from 90% chance of 20-21");
+                    card = getRandomCardExcluding([1]);
+                }
+            } else {
+                if (innerChance <= 0.1) {
+                    // 10% chance to get 21
                     console.log("10% chance to get 21");
                     card = 21 - playerSum;
-                }
-                // rule no.2: 20% chance to get 20
-                else if (inner80 <= 0.3) {
+                } else if (innerChance <= 0.3) {
+                    // 20% chance to get 20
                     console.log("20% chance to get 20");
                     card = 21 - playerSum - 1;
-                }
-                // rule no.3: 30% chance to get any cards that make the sum below 21
-                else if (inner80 <= 0.6) {
+                } else if (innerChance <= 0.6) {
+                    // 30% chance to get any cards that make the sum below 21
                     console.log(
                         "30% chance to get any cards that make the sum below 21"
                     );
-                    let winCard = 21 - playerSum;
-                    let cardIdx = cards.indexOf(winCard);
-                    let tempArr = [];
-                    for (let i = cardIdx; i > 0; i--) {
-                        tempArr.push(cards[i - 1]);
-                    }
-                    //getting the card from tempArr
-                    card = tempArr[Math.floor(Math.random() * tempArr.length)];
-                }
-                // rule no.4: 40% chance to get any cards that make the sum exceeds 21
-                else {
+                    card = getRandomCardBelow(21 - playerSum);
+                } else {
+                    // 40% chance to get any cards that make the sum exceed 21
                     console.log(
-                        "40% chance to get any cards that make the sum exceeds 21"
+                        "40% chance to get any cards that make the sum exceed 21"
                     );
-                    let winCard = 21 - playerSum;
-                    let cardIdx = cards.indexOf(winCard);
-                    let tempArr = [];
-                    for (let i = cardIdx; i < cards.length; i++) {
-                        tempArr.push(cards[i]);
-                    }
-                    //getting the card from tempArr
-                    card = tempArr[Math.floor(Math.random() * tempArr.length)];
-                }
-            }
-            // if the player sum is 20
-            else {
-                var sum20chance = Math.random();
-                //10% chance to win directly to 21;
-                if (sum20chance <= 0.1) {
-                    console.log("win from 10% chance");
-                    card = 1;
-                }
-                //90% chance to lose, sum will exceeds 21
-                else {
-                    console.log("lose from 90% chance of 20-21");
-                    let tempArr = [];
-                    for (let i = 1; i < cards.length; i++) {
-                        tempArr.push(cards[i]);
-                    }
-                    //getting the card from tempArr
-                    card = tempArr[Math.floor(Math.random() * tempArr.length)];
+                    card = getRandomCardAbove(21 - playerSum);
                 }
             }
         } else {
+            // 10% chance to lose
             console.log("10% chance to lose");
-            //logic to get the card that has the value more than the winning numbers (e.g. no.3 made the total 21, so get all cards except 3 and below)
-            let winCard = 21 - playerSum;
-            let cardIdx = cards.indexOf(winCard);
-            let tempArr = [];
-            for (let i = cardIdx; i < cards.length; i++) {
-                tempArr.push(cards[i]);
-            }
-            //getting the card from tempArr
-            card = tempArr[Math.floor(Math.random() * tempArr.length)];
+            card = getRandomCardAbove(21 - playerSum);
         }
     }
+
     playerCards.push(card);
     playerSum = sumTotal(playerCards);
-    $("#player-cards").html("<p>" + playerCards + "</p>");
+    $("#player-cards").html(`<p>${playerCards.join(", ")}</p>`);
+};
+
+function getRandomCardExcluding(exclude) {
+    const tempArr = cards.filter((card) => !exclude.includes(card));
+    return tempArr[Math.floor(Math.random() * tempArr.length)];
 }
 
-function getCardAI() {
-    var card = cards[Math.floor(Math.random() * cards.length)];
+function getRandomCardBelow(maxValue) {
+    const tempArr = cards.filter((card) => card < maxValue);
+    return tempArr[Math.floor(Math.random() * tempArr.length)];
+}
+
+function getRandomCardAbove(minValue) {
+    const tempArr = cards.filter((card) => card >= minValue);
+    return tempArr[Math.floor(Math.random() * tempArr.length)];
+}
+
+const getCardAI = () => {
+    const card = cards[Math.floor(Math.random() * cards.length)];
     dealerCards.push(card);
-    $("#dealer-cards").html('<p id="initialAI">' + dealerCards + "</p>");
+    $("#dealer-cards").html(`<p id="initialAI">${dealerCards.join(", ")}</p>`);
     dealerSum = sumTotal(dealerCards);
     $("#dealer-sum").html(dealerSum);
-}
-function initialCard() {
+};
+
+const initialCard = () => {
     var initialChance = Math.random();
 
     //0.8% chance to get a natural mekdiejack - instant win
@@ -170,20 +137,23 @@ function initialCard() {
         }
     }
     $("#player-sum").html(playerSum);
-}
+};
 
-function naturalJack() {
-    playerCards.push(10);
-    playerCards.push(11);
-    $("#player-cards").html("<p>" + playerCards + "</p>");
-}
-function naturalJackAI() {
-    dealerCards.push(10);
-    dealerCards.push(11);
-    $("#dealer-cards").html('<p id="initialAI">?,' + dealerCards[1] + "</p>");
-}
+const naturalJack = () => {
+    playerCards.push(10, 11);
+    playerSum = sumTotal(playerCards);
+    $("#player-cards").html(`<p>${playerCards.join(", ")}</p>`);
+    $("#player-sum").html(playerSum);
+};
 
-function initialAI() {
+const naturalJackAI = () => {
+    dealerCards.push(10, 11);
+    dealerSum = sumTotal(dealerCards);
+    $("#dealer-cards").html(`<p id="initialAI">?,${dealerCards[1]}</p>`);
+    $("#dealer-sum").html(dealerSum);
+};
+
+const initialAI = () => {
     var initChance = Math.random();
     //0.8% chance to get a natural mekdiejack - instant win
     if (initChance < 0.008) {
@@ -210,9 +180,9 @@ function initialAI() {
         $("#dealer-sum").html(dealerSum);
         $("#initialAI").html('<p id="initialAI">' + dealerCards + "</p>");
     }
-}
+};
 
-function initialCheck() {
+const initialCheck = () => {
     if (playerSum == 21 && dealerSum == 21) {
         draw.play();
         $("#result").html("Instant Draw! Natural Draw de MekdieJack ~ ");
@@ -224,11 +194,17 @@ function initialCheck() {
         $("#restart").html("Play again");
         disableBtn();
     }
-}
-function startGame() {
+};
+
+const startGame = () => {
     $(".board").show();
     $(".startgame").hide();
     $(".game-control").css("display", "inline-flex");
+
+    // Enable buttons when the game starts
+    $("#restart").prop("disabled", false);
+    $("#hit").prop("disabled", false);
+    $("#stay").prop("disabled", false);
 
     //get initial 2 cards
     initialCard();
@@ -236,9 +212,9 @@ function startGame() {
 
     //check the initial cards
     initialCheck();
-}
+};
 
-function restartGame() {
+const restartGame = () => {
     $("#restart").html("Restart");
     $("#result").html("");
     $("#hit").prop("disabled", false);
@@ -258,8 +234,9 @@ function restartGame() {
 
     //check the initial cards
     initialCheck();
-}
-function hitBtn() {
+};
+
+const hitBtn = () => {
     getCard();
     playerSum = sumTotal(playerCards);
     $("#player-sum").html(playerSum);
@@ -275,41 +252,43 @@ function hitBtn() {
         $("#restart").html("Play again");
         disableBtn();
     }
-}
-function stay() {
+};
+
+const stay = () => {
     disableBtn();
     //to delay card draw from AI
-    setTimeout(function () {
+    setTimeout(() => {
         aiMoves();
     }, 1000);
-}
+};
+
 // Basically AI moves
-function aiMoves() {
+const aiMoves = () => {
     dealingCard.play();
-    //keep the game going if the dealer sum still less then 21 (to prevent overflowing when dealer is an instant win)
-    if (!(dealerSum == 21 && dealerSum > playerSum)) {
+
+    // Keep the game going if the dealer sum is still less than 21 (to prevent overflowing when dealer is an instant win)
+    if (!(dealerSum === 21 && dealerSum > playerSum)) {
         getCardAI();
 
-        //this is going to the loop because, after click stay btn, the inside of the timeout is clicking the stay button itself,
-        // so it will keep repeating as a recursive function until certain conditions met
+        // This is going to the loop because, after clicking the stay button, the inside of the timeout is clicking the stay button itself,
+        // so it will keep repeating as a recursive function until certain conditions are met
 
-        //AI keep playing if the sum is not equal to randomizer between 15-19
-        var thresholdAI = [15, 16, 17, 18];
+        // AI keeps playing if the sum is not equal to randomizer between 15-19
+        const thresholdAI = [15, 16, 17, 18];
         const idxThreshold = Math.floor(Math.random() * thresholdAI.length);
 
         if (dealerSum <= thresholdAI[idxThreshold]) {
-            setTimeout(function () {
+            setTimeout(() => {
                 aiMoves();
             }, 500);
-        }
-        // stop playing if exceeds 18
-        else {
+        } else {
+            // Stop playing if exceeds 18
             if (dealerSum > playerSum && dealerSum <= 21) {
                 lose.play();
                 $("#result").html("You Lose");
                 $("#restart").html("Play again");
                 disableBtn();
-            } else if (dealerSum == playerSum) {
+            } else if (dealerSum === playerSum) {
                 draw.play();
                 $("#result").html("Draw");
                 $("#restart").html("Play again");
@@ -321,17 +300,16 @@ function aiMoves() {
                 disableBtn();
             }
         }
-    }
-    // stop the game if the dealer wins on the first draw
-    else {
+    } else {
+        // Stop the game if the dealer wins on the first draw
         lose.play();
         $("#result").html("You Lose");
         $("#restart").html("Play again");
         disableBtn();
     }
-}
+};
 
-function disableBtn() {
+const disableBtn = () => {
     $("#hit").prop("disabled", true);
     $("#stay").prop("disabled", true);
     $("#player-sum").html(playerSum);
@@ -342,4 +320,4 @@ function disableBtn() {
     setTimeout(function () {
         $("#restart").prop("disabled", false);
     }, 1000);
-}
+};
